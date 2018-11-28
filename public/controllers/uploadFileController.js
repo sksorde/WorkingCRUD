4767edf1b693cdf17d4b1cca497721655ecfdfa2
Sailesh Sorde <sksorde@rediffmail.com>
2018-11-28 09:26:05 +0000

    sks: 281118

---
+++ public/controllers/uploadFileController.js
@@ -0,0 +1,14 @@
+﻿var fs = require("fs");
+
+module.exports.uploadCsv = function (req, res) {
+    var today = new Date();
+    var date = today.getDate();
+    var tempPath = req.files.file.tempPath;
+    var targetPath = path.resolve('./uploads/' + req.files.file.originalFilename);
+    if (path.extname(req.files.file.name).toLowerCase() === '.csv') {
+        fs.rename(tempPath, './uploads/csv_1', function (err) {
+            if (err) throw err;
+            console.log("Upload completed");
+        });
+    };
+};
