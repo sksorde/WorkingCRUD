e88a23e05b4934cbb3b53b8392b6776b2c1738e5
Sailesh Sorde <sksorde@rediffmail.com>
2018-11-28 09:27:31 +0000

    sks: 281118

---
+++ models/Users.js
@@ -0,0 +1,16 @@
+﻿var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+
+    firstName: { type: String, Required: 'First name cannot be left blank.' },
+
+    lastName: { type: String, Required: 'Last name cannot be left blank.' },
+
+    eMail: { type: String}
+
+});
+
+var Users = mongoose.model('Users', userSchema, 'Users');
+
+module.exports = Users;
